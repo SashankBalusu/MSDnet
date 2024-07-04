2ac1c717c1d1927e7e7d1b8bf02725fa5e57f56b
Sashank Balusu <sashankbalusu@gmail.com>
2024-07-03 22:07:17 -0700

    minor attendance refactoring and encryption

--- js/attendance.js
+++ js/attendance.js
@@ -3,7 +3,8 @@ import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
-  deleteUser
+  deleteUser,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import {
   child,
@@ -32,7 +33,7 @@ function displayCode(qrObj, item, length){
             displayCode(qrObj, item, length)
         }
 
-    }, (Math.random() * (100 - 10) + 10))
+    }, (Math.random() * (20 - 10) + 10))
 
 }
 const app = initializeApp(firebaseConfig);
@@ -40,50 +41,33 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
 const auth = getAuth();
-window.onload = function() {
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in
+    let accessLevel = CryptoJS.AES.decrypt(localStorage.getItem("accessLevel"), "Ngodeinweb").toString(CryptoJS.enc.Utf8);
+    console.log(accessLevel)
+    if (accessLevel == "Member" || accessLevel == "Captain"){
+        const content = document.getElementById("memberCaptainMain")
+        const genCodeButton = document.createElement("button")
+        genCodeButton.textContent = "Generate Code"
+        genCodeButton.id = "generate"
+        const qrcode = document.createElement("div")
+        qrcode.id = "qrcode"
+        content.appendChild(genCodeButton)
+        content.appendChild(qrcode)
+        const qr = new QRCode(qrcode);
 
-    auth.onAuthStateChanged(function(user) {
-      console.log(user);
-      let accessLevel = ""
-      get(child(ref(db), "whitelist")).then((snapshot) => {
-        if (snapshot.exists()){
-            let content = snapshot.val()
-            for (let access in content){
-                for (let person in content[access]){
-                    if (content[access][person] == user["email"]){
-                        accessLevel = access
-                        break
-                    }
-                }
-            }
-        }
-      }).then( () => {
-        console.log(accessLevel)
-        if (accessLevel == "Member" || accessLevel == "Captain"){
-            const content = document.getElementById("memberCaptainMain")
-            const genCodeButton = document.createElement("button")
-            genCodeButton.textContent = "Generate Code"
-            genCodeButton.id = "generate"
-            const qrcode = document.createElement("div")
-            qrcode.id = "qrcode"
-            content.appendChild(genCodeButton)
-            content.appendChild(qrcode)
-            const qr = new QRCode(qrcode);
-
-            genCodeButton.addEventListener("click", function(){
-                displayCode(qr, user["email"], 0)
-                // for (let i = 0; i <= user["email"].length; i++){
-                //     setInterval(displayCode, 1000, qr, user["email"], i)
-                    
-                // }
-            })
-            
-        }
-
-      })
-
+        genCodeButton.addEventListener("click", function(){
+            displayCode(qr, user["email"], 0)
+            // for (let i = 0; i <= user["email"].length; i++){
+            //     setInterval(displayCode, 1000, qr, user["email"], i)
+                
+            // }
+        })
         
-    })
-}
-
-
+    }
+  } else {
+    // User is signed out
+    // ...
+  }
+});
