01f7a058d108052bbaa50e569d4c1d03c2c2ba2d
Sashank Balusu <sashankbalusu@gmail.com>
2024-08-04 22:12:59 -0700

    binary search to order attendance events

--- js/attendance.js
+++ js/attendance.js
@@ -36,10 +36,39 @@ function displayCode(qrObj, item, length){
     }, (Math.random() * (20 - 10) + 10))
 
 }
+function findLoc(el, arr, st, en) { 
+  // if (arr.length == 1){
+  //   if (el > arr[0]){
+  //     return 0
+  //   }
+  //   else {
+  //     return -1
+  //   }
+  // }
+  st = st || 0; 
+  en = en || arr.length; 
+  var pivot = parseInt(st + (en - st) / 2, 10); 
+  if (arr[pivot == el]){
+    return pivot
+  }
+  else if (en - st <= 1){
+    if (arr[pivot] > el){
+      return pivot - 1
+    }
+    else {
+      return pivot
+    }
+  }
+  if (arr[pivot] < el) { 
+      return findLoc(el, arr, pivot, en); 
+  } else { 
+      return findLoc(el, arr, st, pivot); 
+  } 
+}
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const db = getDatabase();
-
+const dbRef = ref(db)
 const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -85,6 +114,76 @@ onAuthStateChanged(auth, (user) => {
         })
         
         
+    }
+    else {
+      const eventDisplay = document.getElementById("eventDisplay")
+      get(child(dbRef, `events`)).then((snapshot) => {
+        if (snapshot.exists()){
+          let events = snapshot.val()
+          let sortedArr = []
+          for (let key in events){
+            let date = events[key]["date"]
+            let dateSplit = date.split("/")
+            console.log(parseInt(dateSplit[0]))
+            if (parseInt(dateSplit[0]) >= 1 && parseInt(dateSplit[0]) <=6){
+              date += "/" + ((new Date().getFullYear()) + 1)
+            }
+            else {
+              date += "/" + new Date().getFullYear()
+            }
+            let currDay = new Date()
+            let eventDate = new Date(date)
+            let Difference_In_Time = eventDate - currDay;
+            
+            let Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
+            if (sortedArr.length == 0){
+              sortedArr.push(Difference_In_Days)
+              const card = document.createElement("div")
+              card.classList.add("card")
+              card.id = key
+              const p = document.createElement("p")
+              p.textContent = key
+              const distance = document.createElement("p")
+              distance.textContent = Difference_In_Days + " Days"
+              distance.style.position = "absolute"
+              distance.style.bottom = 0
+              card.appendChild(p)
+              card.appendChild(distance)
+              
+              eventDisplay.appendChild(card)
+              
+            }
+            else {
+              let loc = findLoc(Difference_In_Days, sortedArr) + 1
+              sortedArr.splice(loc, 0, Difference_In_Days); 
+              const card = document.createElement("div")
+              card.classList.add("card")
+              card.id = key
+              const p = document.createElement("p")
+              p.textContent = key
+              const distance = document.createElement("p")
+              distance.textContent = Difference_In_Days + " Days"
+              distance.style.position = "absolute"
+              distance.style.bottom = 0
+              card.appendChild(p)
+              card.appendChild(distance)
+
+              eventDisplay.insertBefore(card, eventDisplay.childNodes[loc])
+              console.log(key, date)
+
+            }
+            console.log(sortedArr)
+            console.log(key)
+            
+
+
+          }
+          console.log(sortedArr)
+
+        }
+      }).then(() => {
+        eventDisplay.firstChild.classList.add("moving-border")
+      })
     }
   } else {
     // User is signed out
